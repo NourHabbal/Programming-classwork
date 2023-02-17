2628fa30ee919d10e0c7fe9e007dcc1ce95b75c3
Nour <nour.habbal75@outlook.com>
2023-02-17 14:18:34 -0800

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+name = input("What's your name? ")
+object_name = input("Give me a random object: ")
+subject = input("name a subject: ")
+subject_2 = input("name another subject: ")
+
+print(f"{name} threw a {object_name} at the {subject}, then they spoke with the {subject_2}" )
+
+# name -> hi
+# object_name -> nour
+# subject -> window
+# subject -> candles
