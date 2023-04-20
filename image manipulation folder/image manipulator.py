726354d4304e31268328d3f457dea320f0b5714f
Nour <nour.habbal75@outlook.com>
2023-04-19 20:13:40 -0700

    update image manipulator

--- image manipulation folder/image manipulator.py
+++ image manipulation folder/image manipulator.py
@@ -1,9 +1,52 @@
 from PIL import Image
+from PIL import ImageFilter
 import os
 
-image1 = Image.open('image manipulation folder/bingus.jpeg')
+#image1 = Image.open('image manipulation folder/bingus.jpeg')
 
 #image1.show()
+
+"""
+NOTE:
+--------------
+This script uses Python version 3.11.3 with Pillow to work as intented
+
+
+initial idea:
+
+start()
+    > user chooses out of x options
+    > match based on user input
+        a:
+            view images
+                > get filename from user
+                > open file
+        b:
+            change thumbnail size
+                > get filename from user
+                > get user's preferred thumbnail size
+                > set thumbnail size of image
+                > save with selected thumbnail size
+        c: 
+            jpeg to pdf
+                > get filename from user
+                > save and convert to png by changing the filename to have ".png" at the end
+        d: 
+            make an image black and white
+                > get filename from user
+                > save and use filter (refer to video from class)
+        e: 
+            blur an image
+                > get filename from user
+                > save and use filter (refer to video from class) 
+        f: 
+            rotate an image
+                > get filename from user
+                > get amount of degrees to rotate
+                > save and rotate with users input to rotate (refer to video from class)   
+"""
+
+
 
 def jpeg_to_png(): #DONE
     file_name = input("enter the filename) \nexample: bingus.jpeg \n\nchosen image: ")
@@ -77,13 +120,35 @@ def change_size(): # DONE
     
     end()
 
-def blur_image():
-    print("set to mode 3")
+def blur_image(): #BUGGED
+    try:
+        file_name = input("enter the filename \nexample: bingus.jpeg \n\nchosen image: ")
+        image_blur = Image.open("image manipulation folder/" + str(file_name))
+    except:
+        error_prompt = input("it looks like there is no existing file in this directory. Would you like to try again? \n\n[ 1 ] | yes \n[any] | no \ninput: ")
+        match error_prompt:
+            case "1": blur_image()
+            case _: 
+                quit() 
+    try:
+        os.mkdir("image manipulation folder/blurred images")
+    except:
+        pass 
+    
+    while blur_value is not int:
+        try: 
+            blur_value = int(input("input a blur value to determine how much you want to blur your image. \nvalue 4 for scale is enough to blur out most details: "))
+        except:
+            pass
+    try:
+        image_blur.filter(ImageFilter.GaussianBlur(4)).save("image manipulation folder/blurred images/" + "e") # It appears to me that the interpreter cannot find the "ImageFilter" class. 
+    except:
+        print("cannot save file with blur function properly")
     end()
 
 def black_white(): #DONE
     try:
-        file_name = input("enter the filename) \nexample: bingus.jpeg \n\nchosen image: ")
+        file_name = input("enter the filename \nexample: bingus.jpeg \n\nchosen image: ")
         image_colorless = Image.open("image manipulation folder/" + str(file_name))
     except:
         error_prompt = input("it looks like there is no existing file in this directory. Would you like to try again? \n\n[ 1 ] | yes \n[any] | no \ninput: ")
@@ -99,7 +164,7 @@ def black_white(): #DONE
     try:
         image_colorless.convert(mode= 'L').save("image manipulation folder/black-white images/" + (file_name.replace(".jpeg", "(colorless).jpeg")))
     except:
-        pass 
+        pass
     end()
 
 def rotate_image(): #DONE
@@ -109,7 +174,7 @@ def rotate_image(): #DONE
         pass #pass anything if folder already exists
     
     try: 
-        file_name = input("enter the filename) \nexample: bingus.jpeg \n\nchosen image: ")
+        file_name = input("enter the filename \nexample: bingus.jpeg \n\nchosen image: ")
         image_rotate = Image.open("image manipulation folder/" + str(file_name))
     except:
         error_prompt = input("it looks like there is no existing file in this directory. Would you like to try again? \n\n[ 1 ] | yes \n[any] | no \ninput: ")
@@ -135,6 +200,29 @@ def view_image(): #DONE
             case _: quit()
     end()
 
+def transpose_image():
+    try: 
+        file_name = input("enter the filename \nexample: bingus.jpeg \n\nchosen image: ")
+        image_transpose = Image.open("image manipulation folder/" + str(file_name))
+    except:
+        error_prompt = input("it looks like there is no existing file in this directory. Would you like to try again? \n\n[ 1 ] | yes \n[any] | no \ninput: ")
+        match error_prompt:
+            case "1": rotate_image()
+            case _: 
+                quit()
+    try:
+        os.mkdir("image manipulation folder/transposed images")
+    except:
+        pass
+
+    transpose_choice = input("how would you like to transpose your image? \n[1] | flip horizontally \n[2] | flip vertically \n[3] | rotate 90 degrees \n[4] | rotate 180 degrees \n[5] | rotate 270 degrees \n\n input: ")
+    
+    match transpose_choice:
+        case "1":
+            pass
+    #image_transpose = image_transpose.transpose()
+    print("EYO")
+
 def end(): #DONE
     input_choice = 0
     print("\nchoice reset")
@@ -147,7 +235,7 @@ def end(): #DONE
 def start_menu(): #DONE
     input_choice = 0
     while input_choice > 6 or input_choice < 1:
-        input_choice = int(input("Welcome to the image manipulator! What Would you like to do? \n[1] | convert to png \n[2] | change thumbnail size \n[3] | blur an image \n[4] | change to black n' white \n[5] | rotate an image \n[6] | view an image \n\nSelect a value designated to a function: "))
+        input_choice = int(input("Welcome to the image manipulator! What Would you like to do? \n[1] | convert to png \n[2] | change thumbnail size \n[3] | blur an image \n[4] | change to black n' white \n[5] | rotate an image \n[6] | view an image \n[7] | transpose an image \n\nselect a value designated to a function: "))
     # -----------------------------------------------------------
     if input_choice == 1: jpeg_to_png()
     elif input_choice == 2: change_size()
@@ -155,6 +243,7 @@ def start_menu(): #DONE
     elif input_choice == 4: black_white()
     elif input_choice == 5: rotate_image()
     elif input_choice == 6: view_image()
+    elif input_choice == 7: transpose_image()
     else: print("error 1"); return 1
 
 start_menu()
@@ -172,11 +261,11 @@ the project requirements:
 [size 600] // every file saved with this thumbnail size will be sent here ============================ DONE
 [rotated] // send every rotated file here ============================================================ DONE
 [black white] // send every black/white file here ==================================================== DONE
-No folder for blurred files -- SET UP
-[custom change] // look at documentation and add a custom change with its folder
+No folder for blurred files ==================================== FIX IMAGE FILTER CLASS ERROR
+[custom change] ====== transpose an image
 Have option to view every edited image -------------- ?????
-Have no errors for whatever the user inputs on runtime 
-MUST have unique variable and folder names. ESPECIALLY CODE ========================================= DONE
+Have no errors for whatever the user inputs on runtime =============================================== Hit or miss
+MUST have unique variable and folder names. ESPECIALLY CODE ========================================== DONE
 BONUS: Have the script build the folders for you when a new change type is created ---- in progress
 Expected inputs:
 
